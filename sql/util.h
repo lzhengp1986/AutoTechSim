1e9ca8b82b99e619cc05548798a22facf1009054
lzp <lzhengp1986@163.com>
2023-10-16 22:02:18 +0800

    修改图表

--- sql/util.h
+++ sql/util.h
@@ -5,6 +5,7 @@
 typedef boost::random::mt19937 RandGen;
 typedef boost::random::normal_distribution<> NormDist;
 typedef boost::random::beta_distribution<> BetaDist;
+typedef boost::random::bernoulli_distribution<> BernDist;
 typedef boost::random::uniform_int_distribution<> UnifIntDist;
 
 #endif // UTIL_H
